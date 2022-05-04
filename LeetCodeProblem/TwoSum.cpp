04abdfe309db1bfac6b29806a047ef1f79be0460
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-05-04 19:49:21 +0200

    TwoSum Map

--- LeetCodeProblem/TwoSum.cpp
+++ LeetCodeProblem/TwoSum.cpp
@@ -1,17 +1,46 @@
 #include <vector>
+#include <unordered_map>
+
 using std::vector;
 
+/**
+ * @param nums an array of integers
+ * @param target a integer
+ * @return {i,j} so that nums[i]+nums[j]==target and i!=j.
+ */
 class TwoSum {
 public:
-    vector<int> solution(vector<int>& nums, int target) {
+    /**
+     * Brute-force Solution. Complexity O(n^2)
+     */
+
+    vector<int> solutionBruteForce(vector<int>& nums, int target) {
         vector<int> sol(2);
-        for (int i = 0; i != nums.size(); ++i) {
-            for (int j = i+1; j != nums.size(); ++j) {
+        for (int i = 0; i != nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
                 if (nums[i] == target - nums[j]) {
                     sol = { i,j };
                     break;
                 }
             }
+        }
+        return sol;
+    }
+
+    /**
+     * Map Solution. Complexity O(n)
+     */
+    vector<int> solutionMap(vector<int>& nums, int target) {
+        vector<int> sol(2);
+         std::unordered_map<int,int> numsMap;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (numsMap.count(diff)>0) {
+                sol = { i, numsMap[diff] };
+                break;
+            }
+            numsMap[nums[i]] = i;
         }
         return sol;
     }
--- UnitTest/TwoSumTest.cpp
+++ UnitTest/TwoSumTest.cpp
@@ -13,13 +13,32 @@ namespace TwoSumTest
 	{
 	public:
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Case1)
 		{
 			vector<int> nums { 2,7,11,15 };
 			int target = 9;
+			checkSum(nums, target);
+		}
+		TEST_METHOD(Case2)
+		{
+			vector<int> nums{ 3,2,4 };
+			int target = 6;
+			checkSum(nums, target);
+		}
+		TEST_METHOD(Case3)
+		{
+			vector<int> nums{1,3,3};
+			int target = 6;
+			checkSum(nums, target);
+		}
+		void checkSum(vector<int>& nums, int target) {
 			TwoSum twoSum;
-			vector<int> sol = twoSum.solution(nums,target);
+			vector<int> sol = twoSum.solutionBruteForce(nums, target);
+			Assert::AreEqual(target, nums[sol[0]] + nums[sol[1]]);
+			sol = twoSum.solutionMap(nums, target);
 			Assert::AreEqual(target, nums[sol[0]] + nums[sol[1]]);
 		}
 	};
+
+
 }
